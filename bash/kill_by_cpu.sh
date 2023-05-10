5bf819e5ee657071439c46b76bdf7058078097fb
Guanya Peng <guanya.peng@roche.com>
2023-05-10 22:23:41 +0200

    add kill by cpu script

--- bash/kill_by_cpu.sh
+++ bash/kill_by_cpu.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 command='/opt/fireeye/bin/xagt'   # Replace this with your command
-threshold=90                     # Replace this with your threshold
+threshold=60                    # Replace this with your threshold
 
 while true; do
     pids=$(ps aux | grep "$command" | grep -v grep | awk '{print $2}')
     for pid in $pids; do
-        cpu=$(ps -p $pid -o %cpu | awk 'NR>1')
+        cpu=$(top -bn1 -p $pid | tail -n1 | awk '{printf "%d", $9}')
         max_cpu=$(echo "$cpu 0" | tr ' ' '\n' | sort -nr | head -n1)
+        echo $max_cpu
         if (( $(echo "$max_cpu > $threshold" | bc -l) )); then
             echo "CPU usage of $command with PID $pid is high: $max_cpu%"
             kill -9 $pid
         fi
     done
-    sleep 10
+    sleep 1
 done
