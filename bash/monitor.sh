9a4275cc55e3fd907a868cdb7e4339207a57395f
Guanya Peng <guanya.peng@roche.com>
2023-05-10 22:37:37 +0200

    allow decimal comparision

--- bash/monitor.sh
+++ bash/monitor.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Define the thresholds for fan speed, CPU temperature, and CPU usage
-FAN_THRESHOLD=2000  # Adjust the value as per your fan specifications
-TEMP_THRESHOLD=80   # Adjust the value as per your CPU temperature specifications
-CPU_THRESHOLD=90    # Adjust the value as per your CPU usage specifications
-TOP_PROCESSES=3     # Number of top processes to include in the notification
+FAN_THRESHOLD=1200   # Adjust the value as per your fan specifications
+TEMP_THRESHOLD=60    # Adjust the value as per your CPU temperature specifications
+CPU_THRESHOLD=80.0   # Adjust the value as per your CPU usage specifications
 
 # Function to send notification
 send_notification() {
@@ -38,17 +37,17 @@ while true; do
   cpu_usage=$(mpstat 1 1 | awk '/^Average:/{print 100-$NF}')
 
   # Check if fan speed exceeds threshold
-  if (( fan_speed > FAN_THRESHOLD )); then
+  if (( $(bc <<< "$fan_speed > $FAN_THRESHOLD") )); then
     send_notification "Fan speed is high! Current speed: $fan_speed RPM"
   fi
 
   # Check if CPU temperature exceeds threshold
-  if (( cpu_temp > TEMP_THRESHOLD )); then
+  if (( $(bc <<< "$cpu_temp > $TEMP_THRESHOLD") )); then
     send_notification "CPU temperature is high! Current temperature: $cpu_temp°C"
   fi
 
   # Check if CPU usage exceeds threshold
-  if (( cpu_usage > CPU_THRESHOLD )); then
+  if (( $(bc <<< "$cpu_usage > $CPU_THRESHOLD") )); then
     top_processes=$(get_top_processes)
     message="CPU usage is high! Current usage: $cpu_usage%\n\nTop $TOP_PROCESSES processes:\n$top_processes"
     send_notification "$message"
