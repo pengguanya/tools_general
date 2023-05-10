81f592e0f392ffba027c02b0192065389eca6a59
Guanya Peng <guanya.peng@roche.com>
2023-05-10 22:39:20 +0200

    use awk instead of bc

--- bash/monitor.sh
+++ bash/monitor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Define the thresholds for fan speed, CPU temperature, and CPU usage
-FAN_THRESHOLD=1200   # Adjust the value as per your fan specifications
-TEMP_THRESHOLD=60    # Adjust the value as per your CPU temperature specifications
-CPU_THRESHOLD=80.0   # Adjust the value as per your CPU usage specifications
+FAN_THRESHOLD=2000   # Adjust the value as per your fan specifications
+TEMP_THRESHOLD=80    # Adjust the value as per your CPU temperature specifications
+CPU_THRESHOLD=90.0   # Adjust the value as per your CPU usage specifications
 
 # Function to send notification
 send_notification() {
@@ -37,17 +37,17 @@ while true; do
   cpu_usage=$(mpstat 1 1 | awk '/^Average:/{print 100-$NF}')
 
   # Check if fan speed exceeds threshold
-  if (( $(bc <<< "$fan_speed > $FAN_THRESHOLD") )); then
+  if (( $(awk 'BEGIN { print '"$fan_speed"' > '"$FAN_THRESHOLD"' }') )); then
     send_notification "Fan speed is high! Current speed: $fan_speed RPM"
   fi
 
   # Check if CPU temperature exceeds threshold
-  if (( $(bc <<< "$cpu_temp > $TEMP_THRESHOLD") )); then
+  if (( $(awk 'BEGIN { print '"$cpu_temp"' > '"$TEMP_THRESHOLD"' }') )); then
     send_notification "CPU temperature is high! Current temperature: $cpu_temp°C"
   fi
 
   # Check if CPU usage exceeds threshold
-  if (( $(bc <<< "$cpu_usage > $CPU_THRESHOLD") )); then
+  if (( $(awk 'BEGIN { print '"$cpu_usage"' > '"$CPU_THRESHOLD"' }') )); then
     top_processes=$(get_top_processes)
     message="CPU usage is high! Current usage: $cpu_usage%\n\nTop $TOP_PROCESSES processes:\n$top_processes"
     send_notification "$message"
@@ -55,4 +55,3 @@ while true; do
 
   sleep 1  # Adjust the sleep duration as per your requirement
 done
-
