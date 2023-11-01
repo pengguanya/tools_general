ccda5f55a4b5e68d236f45ebbb25eb2af46dc5a2
Guanya Peng <guanya.peng24@gmail.com>
2023-11-01 23:37:03 +0100

    update dmenu script header and comments

--- bash/dmscripts/dm-confedit.sh
+++ bash/dmscripts/dm-confedit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Script to edit config file from dmenu
+
 terminal="alacritty -e"
 dmeditor="nvim"
 
@@ -22,12 +24,6 @@ declare -a options=(
 # Format the sorted array for dmenu with two columns
 formatted_options=$(printf "%s\n" "${options[@]}" | awk -F' - ' '{printf "%-20s%s\n", $1, $2}')
 
-# Print each option from the sorted array on separate lines
-# for option in "${sorted_options[@]}"; do
-#   echo "$option"
-# done
-
-#echo "${sorted_options[@]}"
 # Pipe the formatted array to dmenu
 selected_option=$(echo -e "$formatted_options" | dmenu -b -i -l 30 -p "Edit Config:")
 
