a7e84b6d1bcbcb00b66ca72c11c2910365635745
Guanya Peng <guanya.peng24@gmail.com>
2024-01-26 22:39:58 +0100

    add header for get_audio_dev

--- bash/get_audio_dev.sh
+++ bash/get_audio_dev.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+################################################################################
+# get_current_audio.sh - Script for identifying current audio input and output settings.
+#
+# This script identifies if the current audio input and output configurations
+# of your system are for a specific speaker setup or headset.
+#
+# It compares the active input/output sink/source with predefined strings
+# to determine if the active configuration matches the speaker or headset parameters.
+#
+# Usage: get_current_audio.sh
+# NOTE: This script does not take any command line arguments.
+#
+# IMPORTANT: This script makes an assumption about the naming of your audio devices.
+# Please note that if you change your headset or speaker setup for your system, 
+# your device names may also change. In such cases, this script may fail to accurately 
+# identify the current audio setup.
+# The predefined strings in the script should be updated to reflect the new device
+# names should this occur.
+#
+# Author: Guanya Peng 
+# Date: January 26, 2024
+################################################################################
 
 # Define variables for sound configuration
 speaker_sink="skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink"
